0306ff90f2994f16590fecf9a42edab1f620c8a5
Nathan Dennis <nathandennis@g.harvard.edu>
2024-10-09 17:13:18 -0400

    Edited main.py to test ELO class

--- src/main.py
+++ src/main.py
@@ -1,17 +1,24 @@
 from simulation import Simulation
+from get_tennis_data import GetTennisData
+from elo_calculations import ELO
 import pandas as pd
 
 def main():
     player_elos = pd.read_csv('player_elos.csv', index_col = 'Player_Name')
     data = pd.read_csv('tennis_data.csv')
 
-    simulation = Simulation(player_elos)
+    elo = ELO()
+    elo.final_elo_csv()
 
-    initial_draw = simulation.find_initial_draw(data, 2024, 'Australian Open')
+    #tennis_data = GetTennisData()
 
-    results = simulation.simulate_tournament(initial_draw, 'Hard')
+    #tennis_data.get_data()
 
-    print(results)
+    #simulation = Simulation(player_elos)
+
+    #initial_draw = simulation.find_initial_draw(data, 2024, 'Australian Open')
+
+    #results = simulation.simulate_tournament(initial_draw, 'Hard')
 
 if __name__ == "__main__":
     results = main()
