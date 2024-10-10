9e322ca8d3856222b9800405c3b213eeb7674ec9
John Breedis <johncbreedis@gmail.com>
2024-10-10 17:30:13 -0400

    Implemented bar plot method to make direct comparison between predicted and actual results

--- src/error_metrics.py
+++ src/error_metrics.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Each method takes in numpy-like input vectors x and y
 
@@ -13,6 +14,36 @@ def Linf(x,y):
 # Computes the average of absolute differences (L_1)
 def L1(x,y):
     return np.average( np.absolute( x-y ) )
+
+
+def barPlot(names, preds, actual):
+    """ Plots a bar-plot comparing predicted probabilities to those
+    obtained via betting odds.
+
+    Input:
+    names (list): list of names for each player
+    preds (list): predicted probabilities
+    actual (list): actual probabilities
+    """
+    # Create plots
+    fig, ax = plt.subplots(figsize=(16,8), layout='constrained')
+    bar_width = 0.4
+    idx = np.arange(len(names))
+
+    # Plot values
+    b1 = ax.bar(idx,             preds, bar_width, label='Predictions')
+    b2 = ax.bar(idx + bar_width, actual, bar_width, label='Actual')
+
+    # Touch-up plot labels
+    ax.set_title('Predicted Prob. vs. Betting Odds')
+    ax.set_xlabel('Name')
+    ax.set_ylabel('Probabilities')
+    ax.set_xticks(idx + bar_width/2)
+    ax.set_xticklabels(names, rotation=55)
+    ax.legend()
+    ax.grid(alpha=0.4)
+
+    plt.show()
 
 
 def displayErrors(preds, actual, display=True):
