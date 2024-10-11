873b008a428d28a71163772e45ac9d6c822b814e
Nathan Dennis <nathandennis@g.harvard.edu>
2024-10-11 14:30:12 -0400

    Added code comments

--- src/plot.py
+++ src/plot.py
@@ -4,9 +4,17 @@ import pandas as pd
 
 class Plot():
     def __init__(self):
-        pass
+        self.bar_width = 0.3
 
     def plots(self, tournament_name):
+        """
+        Creates the plotting function comparing the model's winning probabilities against the betting odds.
+        A side by side bar plot comparing the top 10 players according to the betting odds is plot against the 
+        models probabilities. Saves plot to a png
+
+        Args:
+            tournament_name (str): Name of tournament.
+        """
 
         tournament_name_underscore = tournament_name.replace(' ', '_')
         odds = pd.read_csv(f'../data/2023_{tournament_name_underscore}_Prob.csv', index_col=0)
@@ -16,11 +24,10 @@ class Plot():
         top_players = odds.nlargest(10, 'Normalized Winning Probability')
         top_champions = top_players[['Normalized Winning Probability']].join(data_fr['Champion'], how='inner')
 
-        bar_width = 0.3
         x = np.arange(len(top_champions.index))  # The label locations
 
-        plt.bar(x - bar_width/2, top_champions['Normalized Winning Probability'], width=bar_width, label='Odds Win Probability', color='blue')
-        plt.bar(x + bar_width/2, top_champions['Champion'], width=bar_width, label='Model Win Probability', color='orange')
+        plt.bar(x - self.bar_width/2, top_champions['Normalized Winning Probability'], width=self.bar_width, label='Odds Win Probability', color='blue')
+        plt.bar(x + self.bar_width/2, top_champions['Champion'], width=self.bar_width, label='Model Win Probability', color='orange')
 
         plt.xlabel('Player Name')
         plt.ylabel('Probability')
@@ -29,5 +36,5 @@ class Plot():
         plt.legend()
 
         plt.tight_layout()
-        plt.savefig(f'../imgs/{tournament_name}_plot.png', bbox_inches='tight')
+        plt.savefig(f'../imgs/{tournament_name_underscore}_plot.png', bbox_inches='tight')
         plt.show()
