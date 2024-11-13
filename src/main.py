a671942a92dd7660d386053774f2c384fe454601
Nathan Dennis <nathandennis@g.harvard.edu>
2024-11-13 13:46:48 -0500

    Tested code through main

--- src/main.py
+++ src/main.py
@@ -4,14 +4,16 @@ from elo_calculations import ELO
 from plot import Plot
 from error_metrics import Errors
 from Odds_to_Prob import Odds
+from past_matches import past_match_data
 import pandas as pd
 
 def main():
     tennis_data = GetTennisData()
-    plot = Plot()
+    plot = Plot([0.05, 0.1, 0.5])
     error = Errors()
-    elo = ELO(1500)
+    elo = ELO(1500, 2023)
     odds = Odds()
+    matches = past_match_data()
 
     tennis_data.get_data(year_lower = 2014, year_upper = 2024)
 
@@ -20,15 +22,17 @@ def main():
     player_elos = pd.read_csv('../data/player_elos.csv', index_col = 'Player_Name')
     data = pd.read_csv('../data/tennis_data.csv')
 
-    simulation = Simulation(player_elos, S = 800)
+    matches.win_percentage_common_opponents(data)
 
-    simulation.user_tournament_simulation(data, 2023, 'Wimbledon', 5000)
+    simulation = Simulation(player_elos, S = 800, hth = True, k = 0.5)
 
-    odds.convert_odds(2023, 'Wimbledon')
+    simulation.user_tournament_simulation(data, 2023, 'Australian Open', 5000)
+
+    odds.convert_odds(2023, 'Australian Open')
 
     plot.plots('Wimbledon', 2023)
 
-    error.displayErrors('Wimbledon')
+    error.displayErrors('Wimbledon', [0.1, 0.5])
 
 
 
