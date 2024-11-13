18a99f40c74846c3c899a51bb6b9a32001950ca0
Nathan Dennis <nathandennis@g.harvard.edu>
2024-11-13 13:25:54 -0500

    Edited code comments and added error metrics for new H2H models

--- src/error_metrics.py
+++ src/error_metrics.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 
 class Errors():
+    """
+    Class to calculate error metrics based on the models predicted probabilities and the given winning probabilities based on the odds.
+    """
     def __init__(self):
         pass
 
@@ -45,33 +48,62 @@ class Errors():
         return np.mean(np.absolute(true-pred))
 
 
-    def displayErrors(self, tournament_name, display=True):
+    def displayErrors(self, tournament_name, k_list = None, display=True):
         """ 
         Given prediction probabilities, returns all the utilized error metrics including RMSE, L-Infinity Norm,
         and L-1 Norm.
 
         Input:
             tournament_name (str): Name of tennis tournament
-            display (boolean): Display calculated error metric values, default is True.
+            k_list (None or list): The list of different k factors used in the H2H model to calculate error rates for. This can be none if the H2H model is not being used.
+            display (boolean): Display calculated error metric values, default is True to display errors, False to not display errors.
 
         Output:
-            (double) (x3): RMSE, Linf, and L1 metrics
+            Printed RMSE, Linf, and L1 metrics for given model(s).
         """
+        self.k_list = k_list
+        self.bar_width = 0.3
+        # Only stores k if the k_list was given, if it is None there are no k's
+        if self.k_list is not None:
+            for i, k in enumerate(self.k_list):
+                setattr(self, f'k{i + 1}', k)
+            self.hth = True
+            self.bar_width = 0.15
+        else:
+            self.hth = False
 
-        tournament_name = tournament_name.replace(' ', '_')
-        odds = pd.read_csv(f'../data/2023_{tournament_name}_Prob.csv', index_col=0)
+        tournament_name_underscore = tournament_name.replace(' ', '_')
+        odds = pd.read_csv(f'../data/2023_{tournament_name_underscore}_Prob.csv', index_col=0)
 
-        model = pd.read_csv(f'../data/tournament_results_{tournament_name}.csv', index_col = 0)
+        model = pd.read_csv(f'../data/tournament_results_{tournament_name_underscore}.csv', index_col = 0)
 
-        odds_comparison = odds[['normalized_winning_probability']].join(model['Champion'], how='inner')
-        odds_comparison = odds_comparison.dropna()
+        csv_dict_k = {}
+        csv_dict_k['original'] = model
+        if self.k_list is not None:
+            for k in self.k_list:
+                csv_dict_k[k] = pd.read_csv(f'../data/tournament_results_{tournament_name_underscore}_head_to_head_{k}.csv', index_col=0)
 
+        odds_comparison = odds[['normalized_winning_probability']]
         actual = odds_comparison['normalized_winning_probability']
 
-        preds = odds_comparison['Champion']
-        rmse = self.RMSE(preds, actual)
-        linf = self.Linf(preds, actual)
-        l1 = self.L1(preds, actual)
+        for model_name, model_df in csv_dict_k.items():
+            # Take the Champion column corresponding to the current model to append to odds.
+            champion_column = model_df[f'Champion']
+            
+            odds_comparison[f'Champion_{model_name}'] = champion_column
 
+        # Display only if display is True.
         if display:
-            print("RMSE: {:0.5f} \nL_inf: {:0.5f}\nL_1: {:0.5f}".format(rmse, linf, l1))
+            for model_name, model_df in csv_dict_k.items():
+                champion_column = odds_comparison[f'Champion_{model_name}']
+                    
+                rmse_value = self.RMSE(actual, champion_column)
+                linf_value = self.Linf(actual, champion_column)
+                l1_value = self.L1(actual, champion_column)
+
+                # Print results for the current model
+                print(f"Model: {model_name}")
+                print(f"  RMSE: {rmse_value}")
+                print(f"  Linf: {linf_value}")
+                print(f"  L1: {l1_value}")
+                print('-' * 20)  # Used to separate the output
