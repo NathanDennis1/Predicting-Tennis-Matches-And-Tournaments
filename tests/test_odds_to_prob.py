96d313768acf782109265c4c2d266a7cf99da14c
Nathan Dennis <nathandennis@g.harvard.edu>
2024-11-14 20:08:26 -0500

    Added tests and comments

--- tests/test_odds_to_prob.py
+++ tests/test_odds_to_prob.py
@@ -1,14 +1,38 @@
 import pytest
-from Odds_to_Prob import Odds
+from src.Odds_to_Prob import Odds
+import os
 
 @pytest.fixture
 def odds():
     return Odds()
 
-def test_american_odds_to_probability_positive(odds):
-    probability = odds.american_odds_to_probability(150)
-    assert 0 < probability < 1, "Probability should be between 0 and 1 for positive odds"
+class Test_odds_to_prob():
+    """
+    Class to test the Odds_to_Prob python script
+    """
 
-def test_american_odds_to_probability_negative(odds):
-    probability = odds.american_odds_to_probability(-150)
-    assert 0 < probability < 1, "Probability should be between 0 and 1 for negative odds"
+    def test_american_odds_to_probability_positive(self, odds):
+        """
+        Tests to ensure that given positive odds, the resulting probability is between 0 and 1
+        """
+        probability = odds.american_odds_to_probability(150)
+        assert 0 < probability < 1, "Probability should be between 0 and 1 for positive odds"
+
+    def test_american_odds_to_probability_negative(self, odds):
+        """
+        Tests to ensure that given negative odds, the resulting probability is between 0 and 1
+        """
+        probability = odds.american_odds_to_probability(-150)
+        assert 0 < probability < 1, "Probability should be between 0 and 1 for negative odds"
+
+    def test_convert_odds(self):
+        """
+        Tests to check that a file is created for the convert_odds function.
+        """
+        year = 2023
+        tournament = 'Wimbledon'
+
+        file_path = os.path.join(os.path.dirname(__file__), '..', 'data', f'{year}_{tournament.replace(" ", "_")}_Prob.csv')
+
+        assert os.path.exists(file_path), f"File {file_path} does not exist in the expected location."
+
