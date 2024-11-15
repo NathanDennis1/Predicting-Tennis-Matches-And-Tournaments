ace557c703f2906a70b548b8e2d303ca7e07f780
Nathan Dennis <nathandennis@g.harvard.edu>
2024-11-14 20:02:41 -0500

    Tests for error metrics:

--- tests/test_error_metrics.py
+++ tests/test_error_metrics.py
@@ -1,18 +1,49 @@
 import pytest
-from error_metrics import Errors
+from src.error_metrics import Errors
 
 @pytest.fixture
 def error_metrics():
     return Errors()
 
-def test_rmse(error_metrics):
-    rmse = error_metrics.RMSE(0.8, 0.75)
-    assert rmse >= 0, "RMSE should always be non-negative"
+class Test_error_metrics():
+    def test_rmse_nonneg(self, error_metrics):
+        """
+        Tests if RMSE returns a non-negative value
+        """
+        rmse = error_metrics.RMSE(0.8, 0.75)
+        assert rmse >= 0, "RMSE should always be non-negative"
 
-def test_linf(error_metrics):
-    linf = error_metrics.Linf(0.8, 0.75)
-    assert linf >= 0, "L-Infinity should always be non-negative"
+    def test_linf_nonneg(self, error_metrics):
+        """
+        Tests if linf returns a non-negative value
+        """
+        linf = error_metrics.Linf(0.8, 0.75)
+        assert linf >= 0, "L-Infinity should always be non-negative"
 
-def test_l1(error_metrics):
-    l1 = error_metrics.L1(0.8, 0.75)
-    assert l1 >= 0, "L1 norm should always be non-negative"
+    def test_l1_nonneg(self, error_metrics):
+        """
+        Tests if l1 returns a non-negative value
+        """
+        l1 = error_metrics.L1(0.8, 0.75)
+        assert l1 >= 0, "L1 norm should always be non-negative"
+
+    def test_rmse_float(self, error_metrics):
+        """
+        Tests if RMSE function returns a float
+        """
+        rmse = error_metrics.RMSE(0.8, 0.75)
+        assert isinstance(rmse, float), f"RMSE should return a float, instead it returned {type(rmse)}"
+
+    def test_linf_float(self, error_metrics):
+        """
+        Tests if linf function returns a float
+        """
+        linf = error_metrics.Linf(0.8, 0.75)
+        assert isinstance(linf, float), f"RMSE should return a float, instead it returned {type(linf)}"
+
+    def test_l1_float(self, error_metrics):
+        """
+        Tests if l1 function returns a float
+        """
+        l1 = error_metrics.L1(0.8, 0.75)
+        assert isinstance(l1, float), f"RMSE should return a float, instead it returned {type(l1)}"
