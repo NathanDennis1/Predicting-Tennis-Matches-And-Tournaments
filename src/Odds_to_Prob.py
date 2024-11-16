50857896e3aa2857f0cf2026ef042f9d4300dd4a
Nathan Dennis <nathandennis@g.harvard.edu>
2024-11-15 19:04:38 -0500

    Edited code functionality and paths

--- src/Odds_to_Prob.py
+++ src/Odds_to_Prob.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 class InvalidTournamentError(ValueError):
         pass
@@ -21,6 +22,12 @@ class Odds():
             return 100 / (odds + 100)
         else:
             return -odds / (-odds + 100)
+    def get_project_root(self):
+        """
+        Returns the root directory of the project, which in our case is team_19. This was done
+        so that the test_odds_to_prob.py test code would work.
+        """
+        return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     def convert_odds(self, year, tournament):
         """
@@ -44,12 +51,25 @@ class Odds():
         if tournament not in grand_slams:
             raise InvalidTournamentError(f'Invalid tournament, must be a Grand Slam: One of ', {grand_slams})
         
+        project_root = self.get_project_root()
+
         tournament = tournament.replace(' ', '_')
-        odds_df = pd.read_csv(f'../data/{year}_{tournament}_Odds.csv')
+
+        odds_file = os.path.join(project_root, 'data', f'{year}_{tournament}_Odds.csv')
+
+        # Check for the odds file
+        if not os.path.exists(odds_file):
+            raise FileNotFoundError(f"The odds file {odds_file} does not exist")
+        
+    
+        odds_df = pd.read_csv(odds_file)
 
         odds_df['winning_probability'] = odds_df['Betting Odds'].apply(self.american_odds_to_probability) * 100
 
         total_probability = odds_df['winning_probability'].sum()
         odds_df['normalized_winning_probability'] = (odds_df['winning_probability'] / total_probability)
 
-        odds_df.to_csv(f'../data/{year}_{tournament}_Prob.csv', index=False)
+        data_directory = os.path.join(project_root, 'data')
+
+        output_file = os.path.join(data_directory, f'{year}_{tournament}_Prob.csv')
+        odds_df.to_csv(output_file, index=False)
