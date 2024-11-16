d00cbe407782844581dc4598cef37af10d593de9
Nathan Dennis <nathandennis@g.harvard.edu>
2024-11-15 19:08:05 -0500

    Edited comments for testing odds and function to check for csv

--- tests/test_odds_to_prob.py
+++ tests/test_odds_to_prob.py
@@ -25,14 +25,16 @@ class Test_odds_to_prob():
         probability = odds.american_odds_to_probability(-150)
         assert 0 < probability < 1, "Probability should be between 0 and 1 for negative odds"
 
-    def test_convert_odds(self):
+    def test_convert_odds(self, odds):
         """
-        Tests to check that a file is created for the convert_odds function.
+        Tests to check that a file is created for the convert_odds function. Since it doesn't specifically return a dataframe,
+        we check to ensure the csv file it should create is created.
         """
         year = 2023
         tournament = 'Wimbledon'
+        odds.convert_odds(year, tournament)
 
         file_path = os.path.join(os.path.dirname(__file__), '..', 'data', f'{year}_{tournament.replace(" ", "_")}_Prob.csv')
 
-        assert os.path.exists(file_path), f"File {file_path} does not exist in the expected location."
+        assert os.path.exists(file_path), f"File {file_path} does not exist."
 
