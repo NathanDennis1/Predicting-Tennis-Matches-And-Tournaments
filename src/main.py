193a194d1579f51b47fd3d6e6169a870dd5c0a7e
Nathan Dennis <nathandennis@g.harvard.edu>
2024-11-15 19:05:09 -0500

    Edited main code, changed what to run

--- src/main.py
+++ src/main.py
@@ -9,30 +9,36 @@ import pandas as pd
 
 def main():
     tennis_data = GetTennisData()
-    plot = Plot([0.05, 0.1, 0.5])
+    plot = Plot()
     error = Errors()
     elo = ELO(1500, 2023)
     odds = Odds()
     matches = past_match_data()
 
-    #tennis_data.get_data(year_lower = 2014, year_upper = 2024)
+    tennis_data.get_data(year_lower = 2014, year_upper = 2024)
 
-    #elo.final_elo_csv()
+    data = pd.read_csv('../data/tennis_data.csv')
 
-    #player_elos = pd.read_csv('../data/player_elos.csv', index_col = 'Player_Name')
-    #data = pd.read_csv('../data/tennis_data.csv')
+    elo.final_elo_csv(data)
 
-    #matches.win_percentage_common_opponents(data)
+    player_elos = pd.read_csv('../data/player_elos.csv', index_col = 'Player_Name')
 
-    #simulation = Simulation(player_elos, S = 800, hth = True, k = 0.5)
+    matches.win_percentage_common_opponents(data)
 
-    #simulation.user_tournament_simulation(data, 2023, 'Australian Open', 5000)
+    win_percentage_df = pd.read_csv('../data/win_percentage.csv', index_col='Player_Name')
 
-    #odds.convert_odds(2023, 'Australian Open')
+    games_played = pd.read_csv('../data/games_played_opponents.csv', index_col='Player_Name')
 
-    plot.plots('Wimbledon', 2023)
+    simulation = Simulation(player_elos, S = 800, hth = True, k = 0.1)
 
-    error.displayErrors('Wimbledon', [0.1, 0.5])
+    simulation.simulation_params(win_percentage_df, games_played)
+    simulation.user_tournament_simulation(data, 2023, 'Australian Open', 5000, saves = True)
+
+    odds.convert_odds(2023, 'Australian Open')
+
+    plot.plots('Australian Open', 2023, [0.05, 0.1, 0.5])
+
+    error.displayErrors('Australian Open', [0.05, 0.1, 0.5])
 
 
 
