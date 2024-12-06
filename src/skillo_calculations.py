851d14c72fe80197d0e25d0eb4271d20eca1aab3
Nathan Dennis <nathandennis@g.harvard.edu>
2024-12-06 17:59:42 -0500

    Created intitial skills function to give all players pre-defined initial skills and variances

--- src/skillo_calculations.py
+++ src/skillo_calculations.py
@@ -24,3 +24,25 @@ class skillO:
 
         # Initializes mock ELO class to import functions over
         self.elo_instance = ELO(1500, current_year)
+
+    def initial_skills(self, surfaces, names):
+        """
+        Creates initial skill dataframe with skill mean and variance for each player.
+        
+        Args:
+            surfaces (list): A list of surfaces players are playing on. (Clay, Grass, Hard)
+            names (list): Names of all players.
+
+        Returns:
+            Dataframe containing initial skill (mean) and uncertainty (variance) for each player as a pandas dataframe.
+        """
+        # Create dictionary for player skills and uncertainties (mean, variance)
+        skill_dict = {}
+        for surface in surfaces:
+            skill_dict[f"{surface}_mean"] = [self.initial_mean] * len(names)
+            skill_dict[f"{surface}_variance"] = [self.initial_variance] * len(names)
+
+        # Create dataframe
+        skill_df = pd.DataFrame(skill_dict, index=names)
+        self.skill_dataframe = skill_df
+        return skill_df
