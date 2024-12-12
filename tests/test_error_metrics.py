839dae32c89f0a50ae9096c348660acd3f3e48d2
Nathan Dennis <nathandennis@g.harvard.edu>
2024-12-12 14:08:12 -0500

    Added tests for the new functions written in error_metrics and tested all pytest TypeError arguments from error_metrics code. Also included new test for display errors to ensure it returns a pandas dataframe with mock patch from homework 3

--- tests/test_error_metrics.py
+++ tests/test_error_metrics.py
@@ -1,5 +1,6 @@
 import pytest
 import pandas as pd
+from unittest.mock import patch
 from src.error_metrics import Errors
 
 
@@ -31,17 +32,45 @@ class Test_error_metrics():
 
     def test_rmse_float(self, error_metrics):
         """
-        Tests if RMSE function returns a float
+        Tests if RMSE function returns a float.
         """
         rmse = error_metrics.RMSE(pd.Series(0.8), pd.Series(0.75))
         assert isinstance(rmse, float), f"RMSE should return a float, instead it returned {type(rmse)}"
+    
+    def test_rmse_type_error_true(self, error_metrics):
+        """
+        Test that RMSE raises TypeError if the 'true' argument is not a pandas Series.
+        """
+        with pytest.raises(TypeError):
+            error_metrics.RMSE([0.8, 0.9], pd.Series([0.75, 0.85])) 
+
+    def test_rmse_type_error_pred(self, error_metrics):
+        """
+        Test that RMSE raises TypeError if the 'pred' argument is not a pandas Series.
+        """
+        with pytest.raises(TypeError):
+            error_metrics.RMSE(pd.Series([0.8, 0.9]), [0.75, 0.85])
 
     def test_linf_float(self, error_metrics):
         """
-        Tests if linf function returns a float
+        Tests if Linf function returns a float
         """
         linf = error_metrics.Linf(pd.Series(0.8), pd.Series(0.75))
         assert isinstance(linf, float), f"RMSE should return a float, instead it returned {type(linf)}"
+
+    def test_linf_type_error_true(self, error_metrics):
+        """
+        Test that Linf raises TypeError if the 'true' argument is not a pandas Series.
+        """
+        with pytest.raises(TypeError):
+            error_metrics.Linf([0.8, 0.9], pd.Series([0.75, 0.85])) 
+
+    def test_linf_type_error_pred(self, error_metrics):
+        """
+        Test that Linf raises TypeError if the 'pred' argument is not a pandas Series.
+        """
+        with pytest.raises(TypeError):
+            error_metrics.Linf(pd.Series([0.8, 0.9]), [0.75, 0.85])
 
     def test_l1_float(self, error_metrics):
         """
@@ -49,3 +78,121 @@ class Test_error_metrics():
         """
         l1 = error_metrics.L1(pd.Series(0.8), pd.Series(0.75))
         assert isinstance(l1, float), f"RMSE should return a float, instead it returned {type(l1)}"
+
+    def test_l1_type_error_true(self, error_metrics):
+        """
+        Test that l1 raises TypeError if the 'true' argument is not a pandas Series.
+        """
+        with pytest.raises(TypeError):
+            error_metrics.L1([0.8, 0.9], pd.Series([0.75, 0.85])) 
+
+    def test_l1_type_error_pred(self, error_metrics):
+        """
+        Test that ;1 raises TypeError if the 'pred' argument is not a pandas Series.
+        """
+        with pytest.raises(TypeError):
+            error_metrics.L1(pd.Series([0.8, 0.9]), [0.75, 0.85])
+
+    def test_MAPE_float(self, error_metrics):
+        """
+        Tests if MAPE function returns a float
+        """
+        MAPE = error_metrics.MAPE(pd.Series(0.8), pd.Series(0.75))
+        assert isinstance(MAPE, float), f"MAPE should return a float, instead it returned {type(MAPE)}"
+
+    def test_MAPE_type_error_true(self, error_metrics):
+        """
+        Test that MAPE raises TypeError if the 'true' argument is not a pandas Series.
+        """
+        with pytest.raises(TypeError):
+            error_metrics.MAPE([0.8, 0.9], pd.Series([0.75, 0.85])) 
+
+    def test_MAPE_type_error_pred(self, error_metrics):
+        """
+        Test that MAPE raises TypeError if the 'pred' argument is not a pandas Series.
+        """
+        with pytest.raises(TypeError):
+            error_metrics.MAPE(pd.Series([0.8, 0.9]), [0.75, 0.85])
+
+    def test_R_squared_float(self, error_metrics):
+        """
+        Tests if R_squared function returns a float
+        """
+        R_sq = error_metrics.R_squared(pd.Series(0.8), pd.Series(0.75))
+        assert isinstance(R_sq, float), f"R_squared should return a float, instead it returned {type(R_sq)}"
+
+    def test_R_squared_type_error_true(self, error_metrics):
+        """
+        Test that R_squared raises TypeError if the 'true' argument is not a pandas Series.
+        """
+        with pytest.raises(TypeError):
+            error_metrics.R_squared([0.8, 0.9], pd.Series([0.75, 0.85])) 
+
+    def test_R_squared_type_error_pred(self, error_metrics):
+        """
+        Test that R_squared raises TypeError if the 'pred' argument is not a pandas Series.
+        """
+        with pytest.raises(TypeError):
+            error_metrics.R_squared(pd.Series([0.8, 0.9]), [0.75, 0.85])
+
+    @patch('pandas.read_csv')  # Mocking the pandas read_csv function
+    def test_display_errors_returns_dataframe(self, mock_read_csv, error_metrics):
+        """
+        Test that displayErrors returns a pandas DataFrame.
+        """
+        # Prepare mock data
+        mock_odds_data = pd.DataFrame({
+            'normalized_winning_probability': [0.8, 0.75, 0.65]
+        })
+        
+        mock_model_data = pd.DataFrame({
+            'Champion': [1, 2, 1]  # Mocked model predictions (Champion)
+        })
+        
+        # Set the mock read_csv return values
+        mock_read_csv.side_effect = [mock_odds_data, mock_model_data, mock_model_data]  # Assuming 3 calls to read_csv
+
+        # Call the displayErrors method with mocked inputs
+        result_df = error_metrics.displayErrors(
+            rating_system="ELO",
+            tournament_name="Australian Open"
+        )
+
+        # Test that the result is a DataFrame
+        assert isinstance(result_df, pd.DataFrame), f"Expected a DataFrame, but got {type(result_df)}"
+
+
+        # Test that the DataFrame contains the expected columns
+        expected_columns = ['Model', 'RMSE', 'Linf', 'L1', 'MAPE', 'R-squared']
+        assert all(col in result_df.columns for col in expected_columns), f"Missing columns. Expected columns: {expected_columns}, found: {result_df.columns.tolist()}"
+
+    @patch('pandas.read_csv')  # Mocking the pandas read_csv function
+    def test_display_errors_returns_dataframe_k_list(self, mock_read_csv, error_metrics):
+        """
+        Test that displayErrors returns a pandas DataFrame.
+        """
+        # Prepare mock data
+        mock_odds_data = pd.DataFrame({
+            'normalized_winning_probability': [0.8, 0.75, 0.65]
+        })
+        
+        mock_model_data = pd.DataFrame({
+            'Champion': [1, 2, 1]  # Mocked model predictions (Champion)
+        })
+        
+        # Set the mock read_csv return values
+        mock_read_csv.side_effect = [mock_odds_data, mock_model_data, mock_model_data]  # Assuming 3 calls to read_csv
+
+        # Call the displayErrors method with mocked inputs
+        result_df = error_metrics.displayErrors(
+            rating_system="ELO",
+            tournament_name="Australian Open",
+            k_list = [0.1]
+        )
+
+        # Test that the result is a DataFrame
+        assert isinstance(result_df, pd.DataFrame), f"Expected a DataFrame, but got {type(result_df)}"
+
+        # Test that the DataFrame contains the expected columns
+        expected_columns = ['Model', 'RMSE', 'Linf', 'L1', 'MAPE', 'R-squared']
+        assert all(col in result_df.columns for col in expected_columns), f"Missing columns. Expected columns: {expected_columns}, found: {result_df.columns.tolist()}"
