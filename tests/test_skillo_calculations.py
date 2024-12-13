67ec6323fc8ba4216867202a997298e303c1401e
Nathan Dennis <nathandennis@g.harvard.edu>
2024-12-13 11:48:17 -0500

    Edited code documentation

--- tests/test_skillo_calculations.py
+++ tests/test_skillo_calculations.py
@@ -7,6 +7,9 @@ import pandas as pd
 def skillo():
     """
     Created SkillO class for testing
+
+    Returns:
+        Instance of SkillO class.
     """
     return skillO(initial_mean=25, initial_variance = 8.333, current_year = 2023)
 
@@ -14,6 +17,9 @@ def skillo():
 def df():
     """
     Mock dataframe with arbitrary player names.
+
+    Returns:
+        Mock dataframe as a pandas dataframe.
     """
     data = {
         'tourney_name': ['Australian Open', 'French Open', 'Wimbledon', 'US Open', 
@@ -42,10 +48,10 @@ def df():
 @pytest.fixture
 def player_skillo_df():
     """
-    Creates mock player elo dataset
+    Creates mock player SkillO dataset
 
     Returns:
-        Mock dataframe of tennis players elo ratings, given random elo ratings and ages.
+        Mock dataframe of tennis players SkillO ratings, given random SkillO ratings and ages.
     """
     skillo_data = {
         'Player_Name': ['Player_1', 'Player_2', 'Player_3', 'Player_4' ],
@@ -66,11 +72,14 @@ def player_skillo_df():
     
 class Test_skillo_calculations():
     """
-    Class to test the elo_calculations script.
+    Class to test the SkillO_calculations script.
     """
     def test_initial_skillO(self, skillo):
         """
-        Tests the inital skillo function to return a dataframe
+        Tests the inital skillo function to return a dataframe.
+
+        Parameters:
+            skillo (class): An instance of the SkillO class to be tested.
         """
         surfaces = ['Clay', 'Hard']
         names = ['Player', 'Player_2']
@@ -79,21 +88,32 @@ class Test_skillo_calculations():
 
     def test_expected_game_score(self, skillo):
         """
-        Tests the expected game score function to return a float
+        Tests the expected game score function to return a float.
+
+        Parameters:
+            skillo (class): An instance of the SkillO class to be tested.
         """
         game_score = skillo.expected_game_score(float(25), float(24.5), float(5.2), float(5.4))
         assert isinstance(game_score, float), f"Logistic function should return a float, instead returned {type(game_score)}"
 
     def test_skillo_calculation(self, skillo, df, player_skillo_df):
         """
-        Tests the skillo calculation function to return a dataframe
+        Tests the skillo calculation function to return a dataframe.
+
+        Parameters:
+            skillo (class): An instance of the SkillO class to be tested.
         """
         skillo_calc = skillo.skillO_calculation(df, player_skillo_df)
-        assert isinstance(skillo_calc, pd.DataFrame), f"Elo calculation should return a dataframe, instead returned {type(elo_calc)}"
+        assert isinstance(skillo_calc, pd.DataFrame), f"SkillO calculation should return a dataframe, instead returned {type(skillo_calc)}"
 
     def test_final_skillo_csv(self, skillo, tmp_path, df):
         """
-        Tests the final skillo csv function to create a csv in data.
+        Tests the final skillo csv function to create a csv in data..
+
+        Parameters:
+            skillo (class): An instance of the SkillO class to be tested.
+            tmp_path: A temporary directory path provided by pytest to store the generated CSV file.
+            df (pandas dataframe): Mock dataframe of tennis match history data.
         """
         file_path = tmp_path / "skillo.csv"
 
