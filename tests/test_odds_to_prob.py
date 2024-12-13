7db6ea9084da71e437a6abd7cf91bddd1028a337
Nathan Dennis <nathandennis@g.harvard.edu>
2024-12-13 11:40:26 -0500

    Edited documentation for testing the odds to probability code

--- tests/test_odds_to_prob.py
+++ tests/test_odds_to_prob.py
@@ -4,23 +4,31 @@ import os
 
 @pytest.fixture
 def odds():
+    """
+    Class of odds to probability to run tests. Returns Odds class.
+    """
     return Odds()
 
 class Test_odds_to_prob():
     """
     Class to test the Odds_to_Prob python script
     """
-
     def test_american_odds_to_probability_positive(self, odds):
         """
-        Tests to ensure that given positive odds, the resulting probability is between 0 and 1
+        Tests to ensure that given positive odds, the resulting probability is between 0 and 1.
+
+        Parameters:
+            odds (class): An instance of the Odds class to be tested.
         """
         probability = odds.american_odds_to_probability(150)
         assert 0 < probability < 1, "Probability should be between 0 and 1 for positive odds"
 
     def test_american_odds_to_probability_negative(self, odds):
         """
-        Tests to ensure that given negative odds, the resulting probability is between 0 and 1
+        Tests to ensure that given negative odds, the resulting probability is between 0 and 1.
+
+        Parameters:
+            odds (class): An instance of the Odds class to be tested.
         """
         probability = odds.american_odds_to_probability(-150)
         assert 0 < probability < 1, "Probability should be between 0 and 1 for negative odds"
@@ -29,6 +37,9 @@ class Test_odds_to_prob():
         """
         Tests to check that a file is created for the convert_odds function. Since it doesn't specifically return a dataframe,
         we check to ensure the csv file it should create is created.
+
+        Parameters:
+            odds (class): An instance of the Odds class to be tested.
         """
         year = 2023
         tournament = 'Wimbledon'
@@ -41,6 +52,9 @@ class Test_odds_to_prob():
     def test_invalid_year_type(self, odds):
         """
         Test that TypeError is raised when year is not an integer.
+
+        Parameters:
+            odds (class): An instance of the Odds class to be tested.
         """
         with pytest.raises(TypeError):
             odds.convert_odds("2022", "Australian Open")  # passing a string instead of an integer for year
@@ -48,6 +62,9 @@ class Test_odds_to_prob():
     def test_invalid_tournament_type(self, odds):
         """
         Test that TypeError is raised when tournament is not a string.
+
+        Parameters:
+            odds (class): An instance of the Odds class to be tested.
         """
         with pytest.raises(TypeError):
             odds.convert_odds(2022, 1234)  # passing an integer instead of a string for tournament
