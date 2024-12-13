0a88b112a0c1f459091e1c9ec7f36ac4a7461b53
Nathan Dennis <nathandennis@g.harvard.edu>
2024-12-13 11:46:36 -0500

    Edited documentation of testing past matches

--- tests/test_past_matches.py
+++ tests/test_past_matches.py
@@ -6,6 +6,9 @@ from src.past_matches import past_match_data  # Replace with the actual import p
 def sample_data():
     """
     Creates a sample dataset to test the win_percentage_common_opponents method.
+
+    Returns:
+        Sample data as a dataframe.
     """
     # Create a simple dataframe with match data
     data = {
@@ -26,34 +29,41 @@ class TestPastMatchData:
     def test_return_type(self, past_match, sample_data):
         """
         Test that the return type is a tuple of two DataFrames.
+
+        Parameters:
+            past_match (class): An instance of the past_match_data class to be tested.
         """
         win_percentage_df, games_played_df = past_match.win_percentage_common_opponents(sample_data)
         
-        # Check that the return value is a tuple with two dataframes
         assert isinstance(win_percentage_df, pd.DataFrame), f"Expected win_percentage_df to be a DataFrame, but got {type(win_percentage_df)}"
         assert isinstance(games_played_df, pd.DataFrame), f"Expected games_played_df to be a DataFrame, but got {type(games_played_df)}"
     
     def test_index_names(self, past_match, sample_data):
         """
         Test that both dataframes have 'Player_Name' as the index name.
+
+        Parameters:
+            past_match (class): An instance of the past_match_data class to be tested.
+            sample_data (pd dataframe): Mock pandas dataframe to be tested.
         """
         win_percentage_df, games_played_df = past_match.win_percentage_common_opponents(sample_data)
         
-        # Check that both dataframes have 'Player_Name' as index name
         assert 'Player_Name' in win_percentage_df.index.names, "win_percentage_df should have 'Player_Name' as index name"
         assert 'Player_Name' in games_played_df.index.names, "games_played_df should have 'Player_Name' as index name"
 
     def test_data_integrity(self, past_match, sample_data):
         """
-        Test that the values in the dataframes are correct.
+        Test that the values in the dataframes are correct given mock data ensuring that win percentages and games played are right.
+
+        Parameters:
+            past_match (class): An instance of the past_match_data class to be tested.
+            sample_data (pd dataframe): Mock pandas dataframe to be tested.
         """
         win_percentage_df, games_played_df = past_match.win_percentage_common_opponents(sample_data)
         
-        # Check if the win_percentage_df contains expected data
         assert win_percentage_df['Player_1']['Player_2'] == 1.0, "Expected Player_1 to have 100% win rate against Player_2"
         assert win_percentage_df.loc['Player_2', 'Player_3'] == 0.5, "Expected Player_2 to have 50% win rate against Player_3"
         
-        # Check if games_played_df contains expected data
         assert games_played_df.loc['Player_1', 'Player_2'] == 1, "Expected Player_1 and Player_2 to have played 2 games"
         assert games_played_df.loc['Player_2', 'Player_3'] == 2, "Expected Player_2 and Player_3 to have played 1 game"
 
