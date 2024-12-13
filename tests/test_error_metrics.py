c8703c0b6f0fe8d7bba65211ee61a69de24a3904
Nathan Dennis <nathandennis@g.harvard.edu>
2024-12-13 11:36:26 -0500

    Added documentation to test error metrics. I also want to highlight our use of AI for the final 2 tests, where we got assistance writing the test case using a method to mock the reading of the csv file

--- tests/test_error_metrics.py
+++ tests/test_error_metrics.py
@@ -11,21 +11,30 @@ def error_metrics():
 class Test_error_metrics():
     def test_rmse_nonneg(self, error_metrics):
         """
-        Tests if RMSE returns a non-negative value
+        Tests if RMSE returns a non-negative value.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         rmse = error_metrics.RMSE(pd.Series(0.8), pd.Series(0.75))
         assert rmse >= 0, "RMSE should always be non-negative"
 
     def test_linf_nonneg(self, error_metrics):
         """
-        Tests if linf returns a non-negative value
+        Tests if linf returns a non-negative value.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         linf = error_metrics.Linf(pd.Series(0.8), pd.Series(0.75))
         assert linf >= 0, "L-Infinity should always be non-negative"
 
     def test_l1_nonneg(self, error_metrics):
         """
-        Tests if l1 returns a non-negative value
+        Tests if l1 returns a non-negative value.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         l1 = error_metrics.L1(pd.Series(0.8), pd.Series(0.75))
         assert l1 >= 0, "L1 norm should always be non-negative"
@@ -33,6 +42,9 @@ class Test_error_metrics():
     def test_rmse_float(self, error_metrics):
         """
         Tests if RMSE function returns a float.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         rmse = error_metrics.RMSE(pd.Series(0.8), pd.Series(0.75))
         assert isinstance(rmse, float), f"RMSE should return a float, instead it returned {type(rmse)}"
@@ -40,6 +52,9 @@ class Test_error_metrics():
     def test_rmse_type_error_true(self, error_metrics):
         """
         Test that RMSE raises TypeError if the 'true' argument is not a pandas Series.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         with pytest.raises(TypeError):
             error_metrics.RMSE([0.8, 0.9], pd.Series([0.75, 0.85])) 
@@ -47,13 +62,19 @@ class Test_error_metrics():
     def test_rmse_type_error_pred(self, error_metrics):
         """
         Test that RMSE raises TypeError if the 'pred' argument is not a pandas Series.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         with pytest.raises(TypeError):
             error_metrics.RMSE(pd.Series([0.8, 0.9]), [0.75, 0.85])
 
     def test_linf_float(self, error_metrics):
         """
-        Tests if Linf function returns a float
+        Tests if Linf function returns a float.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         linf = error_metrics.Linf(pd.Series(0.8), pd.Series(0.75))
         assert isinstance(linf, float), f"RMSE should return a float, instead it returned {type(linf)}"
@@ -61,6 +82,9 @@ class Test_error_metrics():
     def test_linf_type_error_true(self, error_metrics):
         """
         Test that Linf raises TypeError if the 'true' argument is not a pandas Series.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         with pytest.raises(TypeError):
             error_metrics.Linf([0.8, 0.9], pd.Series([0.75, 0.85])) 
@@ -68,13 +92,19 @@ class Test_error_metrics():
     def test_linf_type_error_pred(self, error_metrics):
         """
         Test that Linf raises TypeError if the 'pred' argument is not a pandas Series.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         with pytest.raises(TypeError):
             error_metrics.Linf(pd.Series([0.8, 0.9]), [0.75, 0.85])
 
     def test_l1_float(self, error_metrics):
         """
-        Tests if l1 function returns a float
+        Tests if l1 function returns a float.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         l1 = error_metrics.L1(pd.Series(0.8), pd.Series(0.75))
         assert isinstance(l1, float), f"RMSE should return a float, instead it returned {type(l1)}"
@@ -82,20 +112,29 @@ class Test_error_metrics():
     def test_l1_type_error_true(self, error_metrics):
         """
         Test that l1 raises TypeError if the 'true' argument is not a pandas Series.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         with pytest.raises(TypeError):
             error_metrics.L1([0.8, 0.9], pd.Series([0.75, 0.85])) 
 
     def test_l1_type_error_pred(self, error_metrics):
         """
-        Test that ;1 raises TypeError if the 'pred' argument is not a pandas Series.
+        Test that l1 raises TypeError if the 'pred' argument is not a pandas Series.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         with pytest.raises(TypeError):
             error_metrics.L1(pd.Series([0.8, 0.9]), [0.75, 0.85])
 
     def test_MAPE_float(self, error_metrics):
         """
-        Tests if MAPE function returns a float
+        Tests if MAPE function returns a float.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         MAPE = error_metrics.MAPE(pd.Series(0.8), pd.Series(0.75))
         assert isinstance(MAPE, float), f"MAPE should return a float, instead it returned {type(MAPE)}"
@@ -103,6 +142,9 @@ class Test_error_metrics():
     def test_MAPE_type_error_true(self, error_metrics):
         """
         Test that MAPE raises TypeError if the 'true' argument is not a pandas Series.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         with pytest.raises(TypeError):
             error_metrics.MAPE([0.8, 0.9], pd.Series([0.75, 0.85])) 
@@ -110,13 +152,19 @@ class Test_error_metrics():
     def test_MAPE_type_error_pred(self, error_metrics):
         """
         Test that MAPE raises TypeError if the 'pred' argument is not a pandas Series.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         with pytest.raises(TypeError):
             error_metrics.MAPE(pd.Series([0.8, 0.9]), [0.75, 0.85])
 
     def test_R_squared_float(self, error_metrics):
         """
-        Tests if R_squared function returns a float
+        Tests if R_squared function returns a float.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         R_sq = error_metrics.R_squared(pd.Series(0.8), pd.Series(0.75))
         assert isinstance(R_sq, float), f"R_squared should return a float, instead it returned {type(R_sq)}"
@@ -124,6 +172,9 @@ class Test_error_metrics():
     def test_R_squared_type_error_true(self, error_metrics):
         """
         Test that R_squared raises TypeError if the 'true' argument is not a pandas Series.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         with pytest.raises(TypeError):
             error_metrics.R_squared([0.8, 0.9], pd.Series([0.75, 0.85])) 
@@ -131,14 +182,24 @@ class Test_error_metrics():
     def test_R_squared_type_error_pred(self, error_metrics):
         """
         Test that R_squared raises TypeError if the 'pred' argument is not a pandas Series.
+
+        Parameters:
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         with pytest.raises(TypeError):
             error_metrics.R_squared(pd.Series([0.8, 0.9]), [0.75, 0.85])
 
-    @patch('pandas.read_csv')  # Mocking the pandas read_csv function
+    @patch('pandas.read_csv') 
     def test_display_errors_returns_dataframe(self, mock_read_csv, error_metrics):
         """
-        Test that displayErrors returns a pandas DataFrame.
+        Test that displayErrors returns a pandas DataFrame and ensures that the `read_csv` function
+        is properly mocked to simulate reading data.
+
+        Parameters:
+            mock_read_csv (MagicMock): The mocked version of `pandas.read_csv`, which is 
+                                    used to simulate the behavior of reading CSV files 
+                                    without actually accessing the file.
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         # Prepare mock data
         mock_odds_data = pd.DataFrame({
@@ -170,6 +231,12 @@ class Test_error_metrics():
     def test_display_errors_returns_dataframe_k_list(self, mock_read_csv, error_metrics):
         """
         Test that displayErrors returns a pandas DataFrame.
+        
+        Parameters:
+            mock_read_csv (MagicMock): The mocked version of `pandas.read_csv`, which is 
+                                    used to simulate the behavior of reading CSV files 
+                                    without actually accessing the file.
+            error_metrics (class): An instance of the error_metrics class to be tested.
         """
         # Prepare mock data
         mock_odds_data = pd.DataFrame({
