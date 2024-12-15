5995b6362c5d64439bd0ef8b3045399d995895c4
Nathan Dennis <nathandennis@g.harvard.edu>
2024-12-15 17:00:58 -0500

    Remove commented out lines

--- src/main.py
+++ src/main.py
@@ -23,24 +23,23 @@ def main():
     skillo.final_csv(data, '../data/skillo_4.csv')
     skillo_df_4 = pd.read_csv('../data/skillo_4.csv', index_col = 'Player_Name')
 
-    #elo.final_elo_csv(data)
-    #player_elos = pd.read_csv('../data/player_elos.csv', index_col = 'Player_Name')
+    elo.final_elo_csv(data)
+    player_elos = pd.read_csv('../data/player_elos.csv', index_col = 'Player_Name')
 
     simulation_1 = Simulation(skillo_df_4, 'skillO', beta = 1)
     simulation_1.user_tournament_simulation(data, 2023, 'Wimbledon', 5000, sim_num = '4', saves = True)
     simulation_1.user_tournament_simulation(data, 2023, 'Roland Garros', 5000, sim_num = '4', saves = True)
     simulation_1.user_tournament_simulation(data, 2023, 'Australian Open', 5000, sim_num = '4', saves = True)
-    #skillo_wimbledon = pd.read_csv(f'../data/tournament_results_Wimbledon_skillO_1.csv', index_col = 0)
+    skillo_wimbledon = pd.read_csv(f'../data/tournament_results_Wimbledon_skillO_1.csv', index_col = 0)
 
-    #simulation_ELO = Simulation(player_elos, 'ELO', S = 800)
-    #simulation_ELO.user_tournament_simulation(data, 2023, 'Wimbledon', 5000, saves = True)
-    #ELO_wimbledon = pd.read_csv(f'../data/tournament_results_Wimbledon_ELO.csv', index_col = 0)
+    simulation_ELO = Simulation(player_elos, 'ELO', S = 800)
+    simulation_ELO.user_tournament_simulation(data, 2023, 'Wimbledon', 5000, saves = True)
+    ELO_wimbledon = pd.read_csv(f'../data/tournament_results_Wimbledon_ELO.csv', index_col = 0)
 
 
     print(error.displayErrors('SkillO', 'Wimbledon', '4'))
     print(error.displayErrors('SkillO', 'Roland Garros', '4'))
     print(error.displayErrors('SkillO', 'Australian Open', '4'))
-    #print(error.displayErrors('ELO', 'Wimbledon'))
     plot.plot_ELO_vs_SkillO('Wimbledon', 2023, '4')
     plot.plot_ELO_vs_SkillO('Roland Garros', 2023, '4')
     plot.plot_ELO_vs_SkillO('Australian Open', 2023, '4')
