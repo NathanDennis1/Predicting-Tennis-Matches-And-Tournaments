16a4ed8ac1764cd78d3cb34aaec837943ccc24f2
Nathan Dennis <nathandennis@g.harvard.edu>
2024-12-15 16:59:39 -0500

    Changed main.py to include example

--- src/main.py
+++ src/main.py
@@ -13,42 +13,36 @@ def main():
     plot = Plot()
     error = Errors()
     elo = ELO(1500, 2023)
-    skillo = skillO(initial_mean=25, initial_variance=8.3333, current_year=2023)
+    skillo = skillO(initial_mean=25, initial_variance=8.3333, current_year=2023, beta = 1, year_decay = 1.1, gamma = 0.1)
     odds = Odds()
     matches = past_match_data()
 
-    tennis_data.get_data(year_lower = 2014, year_upper = 2023)
-
+    #tennis_data.get_data(year_lower = 2014, year_upper = 2024)
     data = pd.read_csv('../data/tennis_data.csv')
 
-    skillo.final_csv(data)
+    skillo.final_csv(data, '../data/skillo_4.csv')
+    skillo_df_4 = pd.read_csv('../data/skillo_4.csv', index_col = 'Player_Name')
 
     #elo.final_elo_csv(data)
-
     #player_elos = pd.read_csv('../data/player_elos.csv', index_col = 'Player_Name')
 
-    #player_elos = pd.read_csv('../data/player_elos.csv', index_col = 'Player_Name')
+    simulation_1 = Simulation(skillo_df_4, 'skillO', beta = 1)
+    simulation_1.user_tournament_simulation(data, 2023, 'Wimbledon', 5000, sim_num = '4', saves = True)
+    simulation_1.user_tournament_simulation(data, 2023, 'Roland Garros', 5000, sim_num = '4', saves = True)
+    simulation_1.user_tournament_simulation(data, 2023, 'Australian Open', 5000, sim_num = '4', saves = True)
+    #skillo_wimbledon = pd.read_csv(f'../data/tournament_results_Wimbledon_skillO_1.csv', index_col = 0)
 
-    player_skillo = pd.read_csv('../data/skillo.csv', index_col = 'Player_Name')
-    #matches.win_percentage_common_opponents(data)
-
-    #win_percentage_df = pd.read_csv('../data/win_percentage.csv', index_col='Player_Name')
-
-    #games_played = pd.read_csv('../data/games_played_opponents.csv', index_col='Player_Name')
-
-    simulation = Simulation(player_skillo, 'skillO', S = 800, hth = False, k = 0.1)
-
-    #simulation.simulation_params(win_percentage_df, games_played)
-    simulation.user_tournament_simulation(data, 2023, 'Wimbledon', 5000, saves = True)
-
-    #odds.convert_odds(2023, 'Wimbledon')
-
-    #plot.plots('Wimbledon', 2023, [0.05, 0.1, 0.5])
-
-    #error.displayErrors('Wimbledon', [0.05, 0.1, 0.5])
+    #simulation_ELO = Simulation(player_elos, 'ELO', S = 800)
+    #simulation_ELO.user_tournament_simulation(data, 2023, 'Wimbledon', 5000, saves = True)
+    #ELO_wimbledon = pd.read_csv(f'../data/tournament_results_Wimbledon_ELO.csv', index_col = 0)
 
 
-
-
+    print(error.displayErrors('SkillO', 'Wimbledon', '4'))
+    print(error.displayErrors('SkillO', 'Roland Garros', '4'))
+    print(error.displayErrors('SkillO', 'Australian Open', '4'))
+    #print(error.displayErrors('ELO', 'Wimbledon'))
+    plot.plot_ELO_vs_SkillO('Wimbledon', 2023, '4')
+    plot.plot_ELO_vs_SkillO('Roland Garros', 2023, '4')
+    plot.plot_ELO_vs_SkillO('Australian Open', 2023, '4')
 if __name__ == "__main__":
     results = main()
