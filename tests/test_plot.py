625793fcdb304adb899619b10462d2afd643b887
Iris Zhou <irisz1@uw.edu>
2024-12-29 20:42:20 -0800

    Add plot parameter tests

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -25,7 +25,7 @@ class TestPlot:
         plot.plots('Wimbledon', 2023, 'skillO', '2')
 
         # This checks to make sure a figure was created
-        fig = plt.gcf() 
+        fig = plt.gcf()
 
         # Assert that the figure is a matplotlib figure
         assert isinstance(fig, plt.Figure), "No figure was created."
@@ -40,7 +40,47 @@ class TestPlot:
         plot.plot_ELO_vs_SkillO('Wimbledon', 2023, '2')
 
         # This checks to make sure a figure was created
-        fig = plt.gcf() 
+        fig = plt.gcf()
 
         # Assert that the figure is a matplotlib figure
         assert isinstance(fig, plt.Figure), "No figure was created."
+
+    def test_invalid_year_type(self, plot):
+        """
+        Test that TypeError is raised when year is not an integer for plots function.
+
+        Parameters:
+            plot (class): An instance of the Plot class to be tested.
+        """
+        with pytest.raises(TypeError):
+            plot.plots('Wimbledon', '2023', 'skillO', '2')  # passing a string isntead of a integer for year
+
+    def test_invalid_year_value(self, plot):
+        """
+        Test that ValueError is raised when year is not between 1968 and 2024 (inclusive) when calling plots function.
+
+        Parameters:
+            plot (class): An instance of the Plot class to be tested.
+        """
+        with pytest.raises(ValueError):
+            plot.plots('Wimbledon', 2026, 'skillO', '2')
+
+    def test_invalid_k_list_type(self, plot):
+        """
+        Test that ValueError is raised when k_list is not a list for plots function.
+
+        Paramters:
+            plot (class): An instance of the Plot class to be tested.
+        """
+        with pytest.raises(ValueError):
+            plot.plots('Wimbledon', 2023, 'skillO', '2', k_list = 5)
+
+    def test_invalid_k_list_type(self, plot):
+        """
+        Test that ValueError is raised when k_list is not a list for plots function.
+
+        Paramters:
+            plot (class): An instance of the Plot class to be tested.
+        """
+        with pytest.raises(ValueError):
+            plot.plots('Wimbledon', 2023, 'skillO', '2', k_list = 5)
